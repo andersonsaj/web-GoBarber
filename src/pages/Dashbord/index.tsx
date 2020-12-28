17b53c7e68d7dff023adb0bc2ce49d5f9e3094ff
andersonsaj <andersonsb09@hotmail.com>
2020-12-28 15:43:26 -0300

    listagem de agendamentos

--- src/pages/Dashbord/index.tsx
+++ src/pages/Dashbord/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiClock, FiPower } from 'react-icons/fi';
 import {
   Container,
@@ -8,12 +8,16 @@ import {
   Content,
   Schedule,
   NextAppointment,
+  Section,
+  Appointment,
   Calendar,
 } from './styles';
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashbord: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const { signOut, user } = useAuth();
   return (
     <Container>
@@ -55,6 +59,41 @@ const Dashbord: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/59414608?s=460&u=a3a32cd080d4c6fa6bd973064c4f3d8dffe56e73&v=4"
+                  alt="Anderson"
+                />
+                <strong>Anderson Barreto</strong>
+              </div>
+            </Appointment>
+          </Section>
+          <Section>
+            <strong>Tarde</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                13:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/59414608?s=460&u=a3a32cd080d4c6fa6bd973064c4f3d8dffe56e73&v=4"
+                  alt="Anderson"
+                />
+                <strong>Anderson Barreto</strong>
+              </div>
+            </Appointment>
+          </Section>
         </Schedule>
         <Calendar />
       </Content>
